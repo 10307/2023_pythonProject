3d26ba0cfff5af33a369de9959161b6e6e732580
User <goodmason@naver..com>
2023-07-29 11:33:43 +0900

    논리연산 학습

--- operator.py
+++ operator.py
@@ -14,3 +14,32 @@ n = 13 // 4
 
 print (r)
 print (n)
+
+#참 / 거짓
+trueValue = True
+falseValue = False
+
+#논리연산
+numA = 20
+numB = 4
+isEqual = numA = numB
+isEqual = 5 == 4
+isnotEqual = 5 !=4
+
+print(isEqual)
+print(isnotEqual)
+
+isEqual = numA == 20
+
+print(isEqual)
+
+
+isBiggerThan = numA > numB
+print(isBiggerThan)
+print(isSmallerOrEqualThan)
+
+#isSmallerThan =
+#isSmallerOrEqualThan =
+
+#print(isSmallerThan)
+#print(isSmallerOrEqualThan)
