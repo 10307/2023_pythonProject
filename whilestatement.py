9d33910761c6677148568b175c065911c7767a32
10307 <139601502+10307@users.noreply.github.com>
2023-10-28 11:00:25 +0900

    Revert "2023/ 9/ 23"

--- whilestatement.py
+++ whilestatement.py
@@ -28,20 +28,19 @@
 import random
 answer = random.randrange(0,10)
 user_input = -1
-chance = 3
 
 # 사용자가 answer 맞출때까지 반복
 # 1. 사용자에게 기회주기(3번)
 # 2. 틀렸을때 updown 출력해주기
-while chance > 0
+while True:
+    user_input = int(input("값을 입력하세요 >>"))
+    if user_input == answer:
+        print("다시 한번 생각해보세요")
     user_input = int(input("값을 입력하세요 >>"))
 
-  if user_input == answer:
+
+    if user_input == answer:
         print("정답입니다!")
         break
-    else:
-        chance = chance -1
-        if user_input > answer:
-            print("down")
-        else:
-            print("up")
+
+#값 맞추기
